e1902ca994bb8d7a8d2f58db4c80d4c62f69e58f
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-10-15 02:38:39 -0300

    agrego ejemplos de clase 18

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, "ejemplo/saludar.html")
+    return render(request, "ejemplo/saludar.html", 
+    {"nombre": "Osvaldo",
+    "apellido": "Valoriani",})
 
-    
+def index_dos(request, nombre, apellido):
+    return render(request, "ejemplo/saludar.html",
+     {"nombre": nombre,
+    "apellido": apellido,})
+
+def index_tres(request):
+    return render(request, "ejemplo/saludar.html",
+    {"notas": [1,2,3,4,5,6,7,8]})
+
+def imc(request, peso, altura):
+    imc = peso/ (altura**2)
+    return render(request, "ejemplo/imc.html",{"imc": imc})
