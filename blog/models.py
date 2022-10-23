7e9ba85026b812ac13fc5f50b4f388fd4a0e419a
Osvaldo Valoriani <osvaldovaloriani@hotmail.com>
2022-10-22 22:03:29 -0300

    se modifican titulos, subtitulos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+class Configuracion(models.Model):
+    nombre_blog = models.CharField(max_length = 14)
+    construido_por = models.CharField(max_length = 30)
+    titulo_principal = models.CharField(max_length =30, default = '', blank = False)
+    subtitulo_principal = models.CharField(max_length = 60, default ='')
+
+
